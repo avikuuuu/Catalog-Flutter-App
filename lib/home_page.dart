2a2e646fe02d56a059531b50063d68d41d92dc7c
aviku11 <70461207+avikuuuu@users.noreply.github.com>
2023-01-13 22:56:00 +0530

    day2

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+    String name = "avki";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catelog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text('welcomé to $days my world $name'),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}

11b3ee8b3baf172dd80b8a958ff44a52a7ee50d4
aviku11 <70461207+avikuuuu@users.noreply.github.com>
2023-01-15 15:17:53 +0530

    day10 complete

---
+++ lib/models/catalog.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+
+class Item {
+  final String id;
+  final String name;
+  final String description;
+  final num price;
+  final String color;
+  final String image;
+  Item({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.color,
+    required this.image,
+  });
+}
+
+final product = [
+  Item(id: "1", name: "apple", description: "over ratted", price: 200000, color: "#33505a", image: "")
+];
